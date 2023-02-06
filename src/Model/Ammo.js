27c364f5868c24bd393caed405a1983ebf3a24d4
Jordan <>
2023-02-06 10:29:41 +0000

    Initial commit

---
+++ src/Model/Ammo.js
@@ -0,0 +1,12 @@
+export class Ammo{
+    Id;
+    Name;
+    Caliber;
+    Damage;
+    constructor(id,name, caliber, damage){
+        this.Id = id;
+        this.Name = name;
+        this.Caliber = caliber;
+        this.Damage = damage;
+    }
+  }
